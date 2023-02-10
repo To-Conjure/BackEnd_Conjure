7bbabc65abcd84306e18b4fbaac832c10cd17541
evanlu1134 <evanlu1134@gmail.com>
2023-02-10 09:33:53 -0500

    added register section

--- model/userModel.js
+++ model/userModel.js
@@ -7,6 +7,7 @@ class User {
     return query.rows;
 }
 
+
 static async getSingleUsersInfoFromDB(id) {
   const db = "SELECT * FROM users JOIN user_info ON users.user_id = user_info.user_id WHERE users.user_id = $1";
   const dbInfo = await pool.query(db, [id]);
@@ -23,6 +24,13 @@ static async getSingleUsersInfoFromDB(id) {
     const db = "SELECT * FROM users JOIN user_info ON users.user_id = user_info.user_id";
     const dbInfo = await pool.query(db);
     return dbInfo.rows;
+  }
+
+  static async registerUserToDB(...args) { 
+    const data = [...args]
+    const db = `INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING user_id, username, email;`
+    const dbInfo = await pool.query(db, data);
+    return dbInfo.rows[0];
   }
 }
 
--- route/userRoute.js
+++ route/userRoute.js
@@ -3,8 +3,11 @@ const userController = require("../controller/userController");
 const router = express.Router();
 
 router.get("/", userController.getAllUsers)
+router.get("/:id", userController.getSingleUser)
 router.get("/stat", userController.getAllUsersInfo)
 router.get("/stat/:id", userController.getSingleUserInfo)
-router.get("/:id", userController.getSingleUser)
+
+router.post("/register", userController.registerUser)
+
 
 module.exports = router
